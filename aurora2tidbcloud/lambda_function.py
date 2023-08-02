393f32e273ee35a65234ba15419290675938c1c7
Debian <admin@ip-172-82-11-174.ec2.internal>
2023-08-02 13:20:21 +0000

    Added the Makefile and convert the source code to OOP

--- aurora2tidbcloud/lambda_function.py
+++ aurora2tidbcloud/lambda_function.py
@@ -8,91 +8,108 @@ import csv
 import subprocess
 
 # rds settings
-user_name = os.environ['USER_NAME']
-password  = os.environ['PASSWORD']
-rds_host  = os.environ['RDS_HOST']
-db_name   = os.environ['DB_NAME']
 DATABASE_LIST = ["test"]
+
 TEMP_BASE_DIR = "/tmp"
 PATH_TEMPLATE = '{base_dir}/{db_host}/{db_name}/{file}'
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# create the database connection outside of the handler to allow connections to be
-# re-used by subsequent function invocations.
-try:
-    conn = pymysql.connect(host=rds_host, user=user_name, passwd=password, db=db_name, connect_timeout=5)
-except pymysql.MySQLError as e:
-    logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
-    logger.error(e)
-    sys.exit()
+class MySQLDump:
+    def __init__(self, rdsHost: str, rdsPort: int, rdsUser: str, rdsPassword: str, s3Bucket: str, s3Key: str, backupDBs: list = None ):
+        self.RDSHost     = rdsHost
+        self.RDSPort     = rdsPort
+        self.RDSUser     = rdsUser
+        self.RDSPassword = rdsPassword
+        self.S3Bucket    = s3Bucket
+        self.S3Key       = s3Key
+        self.BackupDBs   = backupDBs
 
-logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+        # create the database connection outside of the handler to allow connections to be
+        # re-used by subsequent function invocations.
+        try:
+            self.conn = pymysql.connect(host=self.RDSHost, port=self.RDSPort, user=self.RDSUser, passwd=self.RDSPassword, db="mysql", connect_timeout=5)
+        except pymysql.MySQLError as e:
+            logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+            logger.error(e)
+            sys.exit()
+        
+        logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
 
-def upload_s3(fileName: str):
-    s3 = boto3.client('s3')
-    response = s3.upload_file(fileName,  "jay-data", f"lambda/data/{os.path.basename(fileName)}")
-    logger.info(f"upload file response: {response}")
+        if self.BackupDBs is None:
+            self.BackupDBs = self.fetchAllDBs()
 
-def backup() -> bool:
-    logger.info("Starting to backup the database")
-    for db in DATABASE_LIST:
-        localPath = save_file_to_local(db, False)
-        logger.info(f"The exported file: {localPath}")
-        upload_s3(localPath)
+    def upload_s3(self, fileName: str):
+        s3 = boto3.client('s3')
+        response = s3.upload_file(fileName,  self.S3Bucket, f"{self.S3Key}/{os.path.basename(fileName)}")
+        logger.info(f"upload file response: {response}")
+
+    def Backup(self) -> bool:
+        logger.info("Starting to backup the database")
+        for db in self.BackupDBs:
+            localPath = self.save_file_to_local(db, False)
+            logger.info(f"The exported file: {localPath}")
+            self.upload_s3(localPath)
+
+    def fetchAllDBs(self) -> list:
+        targetDBs = []
+        with self.conn.cursor() as cur:
+            cur.execute("select distinct table_schema from information_schema.tables where table_schema not in ('information_schema', 'sys', 'mysql', 'performance_schema')")
+            logger.info("The following items have been added to the database:")
+            for row in cur:
+                targetDBs.append(row[0])
+
+        logger.info(f"All the db are: {targetDBs}")
+        return targetDBs
     
-def save_file_to_local(db: dict, compress: bool=True):
-    exp = 'sql.gz' if compress else 'ddl.sql'
+    def save_file_to_local(self, dbName: str, compress: bool=True):
+        exp = f'{dbName}.sql.gz' if compress else f'{dbName}.ddl.sql'
+    
+        target_db_string = f'-h {self.RDSHost} --port {self.RDSPort} -u {self.RDSUser} -p{self.RDSPassword} {dbName}'
+    
+        target_local_path = PATH_TEMPLATE.format(
+            base_dir=TEMP_BASE_DIR,
+            db_host=self.RDSHost,
+            db_name=dbName,
+            file=f'{exp}'
+        )
+        os.makedirs(os.path.dirname(target_local_path), exist_ok=True)
+    
+        mysqldump_string = f'/opt/bin/mysqldump --no-autocommit=1 --single-transaction=1 --extended-insert=1 {target_db_string}'
+        command = f'{mysqldump_string} | gzip -9 > {target_local_path};' if compress else f'{mysqldump_string} > {target_local_path};'
+    
+        logger.info(f"command: <{command}>")
+    
+        response = subprocess.run(command, shell=True, capture_output=True)
+        if response.returncode != 0:
+            logger.info(f"response: {response.stderr}")
+    
+        return target_local_path
 
-    target_db_string = f'-h {rds_host} --port 3306 -u {user_name} -p{password} {db_name}'
-
-    target_local_path = PATH_TEMPLATE.format(
-        base_dir=TEMP_BASE_DIR,
-        db_host=rds_host,
-        db_name="test",
-        file=f'{exp}'
-    )
-    os.makedirs(os.path.dirname(target_local_path), exist_ok=True)
-
-    mysqldump_string = f'/opt/bin/mysqldump --no-autocommit=1 --single-transaction=1 --extended-insert=1 {target_db_string}'
-    command = f'{mysqldump_string} | gzip -9 > {target_local_path};' if compress else f'{mysqldump_string} > {target_local_path};'
-
-    logger.info(f"command: <{command}>")
-
-    response = subprocess.run(command, shell=True, capture_output=True)
-    if response.returncode != 0:
-        logger.info(f"response: {response.stderr}")
-
-    return target_local_path
 
 def lambda_handler(event, context):
     """
     This function creates a new RDS database table and writes records to it
     """
 
-    backup()
+    msg = ""
+    if "RDSConn" not in event or "rds_host" not in event["RDSConn"] or "rds_port" not in event["RDSConn"] or "rds_user" not in event["RDSConn"] or "rds_password" not in event["RDSConn"]:
+        msg = "Please input the parameters like (eg: {'rds_host': 'localhost', 'rds_port': 3306, 'rds_user': 'rds_user', 'rds_password': 'password'})"
+    if msg != "":
+        return msg
 
-    return "test"
-#    message = event['Records'][0]['body']
-#    data = json.loads(message)
-#    CustID = data['CustID']
-#    Name = data['Name']
-#
-#    upload_s3()
-#
-#    item_count = 0
-#    sql_string = f"insert into Customer (CustID, Name) values({CustID}, '{Name}')"
-#
-#    with conn.cursor() as cur:
-#        cur.execute("create table if not exists Customer ( CustID  int NOT NULL, Name varchar(255) NOT NULL, PRIMARY KEY (CustID))")
-#        cur.execute(sql_string)
-#        conn.commit()
-#        cur.execute("select * from Customer")
-#        logger.info("The following items have been added to the database:")
-#        for row in cur:
-#            item_count += 1
-#            logger.info(row)
-#    conn.commit()
-#
-#    return "Added %d items to RDS MySQL table" %(item_count)
+    if "S3" not in event or "BucketName" not in event["S3"] or "S3Key" not in event["S3"]:
+        msg = "Please inout the parameters like {'S3': {'BucketName': 'bucket name', 'S3Key': 's3 key'}}"
+
+    if msg != "":
+        return msg
+
+    if "TargetDBs" not in event:
+        mysqlDump = MySQLDump(event["RDSConn"]["rds_host"], event["RDSConn"]["rds_port"], event["RDSConn"]["rds_user"], event["RDSConn"]["rds_password"], event["S3"]["BucketName"], event["S3"]["S3Key"])
+    else:
+        mysqlDump = MySQLDump(event["RDSConn"]["rds_host"], event["RDSConn"]["rds_port"], event["RDSConn"]["rds_user"], event["RDSConn"]["rds_password"], event["S3"]["BucketName"], event["S3"]["S3Key"], event["TargetDBs"])
+
+    mysqlDump.Backup()
+
+    return "Successfully dump the ddl"
